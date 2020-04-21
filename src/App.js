28813eb87860c96e58dcd97a440e880b7d0ee431
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2020-04-21 02:45:15 -0500

    signup and login forms

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Home from "./components/Home"
+import Signup from "./components/Signup"
+import Login from "./components/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/signup" component={Signup} exact />
+      <Route path="/login" component={Login} exact />
+      
+      <Route component={Error} />
+    </Switch>
   );
 }
 
